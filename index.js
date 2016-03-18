8c61503e3b19fe8ce4b1798f8e9c07a87f1cf792
Kris Kelley <kelleyblackmore@gmail.com>
2016-03-18 11:53:11 -0700

    Updated button text

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function place_raw_button() {
     var navbar = $(".navbar-default .container-fluid .navbar-right").get(0);
-    $(navbar).append("<button class='btn btn-xs btn-default' id='raw-btn'>Raw</button>");
+    $(navbar).append("<button class='btn btn-xs btn-default' id='raw-btn'>Get Raw</button>");
     $('#raw-btn').bind("click", function() {
         var filename = Jupyter.editor.file_path;
         var raw_path = window.location.href.replace("edit", "files");
